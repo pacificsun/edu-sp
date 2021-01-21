6464fa7e4f48b346a4029619441922ca47d451e6
Suraj Thapa <stsurajst@gmail.com>
2021-01-21 18:06:08 +0545

    store and persistor created

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import createSagaMiddleware from "redux-saga";
+
+import rootReducer from "./root-reducer";
+import rootSaga from "./root-saga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
