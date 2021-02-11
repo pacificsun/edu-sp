6e02e872dcc0d6dea16000e83bb7ee05edfaaa5a
Suraj Thapa <stsurajst@gmail.com>
2021-02-11 22:40:22 +0545

    signup saga

--- src/store/auth/auth.sagas.js
+++ src/store/auth/auth.sagas.js
@@ -1,10 +1,28 @@
+import { API } from "aws-amplify";
 import { takeLatest, call, put, select } from "redux-saga/effects";
+
+import { createUser } from "../../graphql/mutation";
+import { signupFail, signupSuccess } from "./auth.action";
+import { openAlert } from "../alert/alert.action";
 
 import * as AuthType from "./auth.types";
 
 // worker saga
 
-export function* onSignupAsync() {}
+export function* onSignupAsync({ payload: { formData, history } }) {
+  try {
+    yield API.graphql({
+      query: createUser,
+      variables: { input: formData },
+    });
+    yield put(signupSuccess(formData));
+    yield history.push({ pathname: "/verify-code" });
+  } catch (err) {
+    yield put(openAlert(err.message, "error"));
+    console.error(err);
+    yield put(signupFail(err.message));
+  }
+}
 
 // watcher saga
 
